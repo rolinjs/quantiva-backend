260c6d0f1975d60a88990951c2df5216e1febbfd
Rolin Chavez <rolinfullstack@gmail.com>
2025-12-14 23:09:52 -0500

    change gmail 3

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -2,9 +2,9 @@ import nodemailer from 'nodemailer'
 
 export const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     service: "gmail",
-    port: 587,
+    secure: true,
     auth: {
         user: "rolinfullstack@gmail.com",
         pass: "bnyxuighidrqzsan"
